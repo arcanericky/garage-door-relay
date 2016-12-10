a27e21d324c3c0d7fad3cd8e5baad7381dfddffe
Ricky Pike <arcanericky@gmail.com>
2016-12-09 19:30:21 -0600

    Refactoring and functionality improvements

--- smartapps/arcanericky/garage-door-relay.src/garage-door-relay.groovy
+++ smartapps/arcanericky/garage-door-relay.src/garage-door-relay.groovy
@@ -17,46 +17,70 @@ definition(
 
 preferences {
     section("Open garage door when this switch is flipped on") {
-        input "theswitch", "capability.switch", required: true
+        input("theswitch", "capability.switch", required: true)
     }
     
     section("Open this garage door") {
-        input "thedoor", "capability.garageDoorControl", required: true
-        //input "thedoor", "capability.doorControl", required: true
+        input("thedoor", "capability.garageDoorControl", required: true)
     }
-    
-    
-    section("Detect garage door left open?"){
+
+    section("Detect garage door left open?") {
         input("recipients", "contact", title: "Send notifications to") {
-            input "sendPushMessage", "enum", title: "Send a push notification?", options: ["No", "Yes"], required: true, defaultValue: "No"
+            input("sendPushMessage", "enum", title: "Send a push notification?", options: ["No", "Yes"], required: true, defaultValue: "No")
         }
         
-		input "detectOpenTimeout", "number", title: "Garage door left open timeout", required: false, defaultValue: 10
+		input("detectOpenTimeout", "number", title: "Garage door left open timeout", required: false, defaultValue: 30)
 	}
-    
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
+	log.debug("Installed with settings: ${settings}")
 
-	log.debug "sendPushMessage is " + sendPushMessage
 	initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+	log.debug("Updated with settings: ${settings}")
 
 	unsubscribe()
+
+    syncSwitch()
+    
 	initialize()
+    
+    //detectDoorState()
 }
 
 def initialize() {
+	log.trace("initialize()")
+    
+    subscribe(thedoor, "door.open", doorOpenHandler)
+    subscribe(thedoor, "door.closed", doorClosedHandler)
+    subscribe(thedoor, "door.opening", doorOpeningHandler)
+    subscribe(thedoor, "door.closing", doorClosingHandler)
+    subscribe(thedoor, "door.unknown", doorUnknownHandler)
+    
 	subscribe(theswitch, "switch.on", switchOnHandler)
     subscribe(theswitch, "switch.off", switchOffHandler)
+}
+
+def syncSwitch() {
+	log.trace("syncSwitch()")
     
-    subscribe(thedoor, "door", doorHandler)
+    def switchState = theswitch.currentState("switch").value
+    def garageDoorState = getGarageDoorState()
     
-    detectDoorState()
+    log.debug ("Switch state is: " + switchState + ". Garage door state is: " + garageDoorState)
+    
+    if (garageDoorState == "open" && switchState != "on") {
+    	log.debug("Syncing switch to open")
+        
+        theswitch.on()
+    } else if (garageDoorState == "closed" && switchState != "off") {
+    	log.debug("Syncing switch to closed")
+    	
+        theswitch.off()
+    }
 }
 
 // ========
@@ -65,34 +89,94 @@ def initialize() {
 
 def switchOnHandler(evt) {
 	log.trace "switchOnHandler()"
-    logCurrentState()
-	log.debug "Opening garage door"
-    openGarageDoor()
+    
+    if (getGarageDoorState() != "open") {
+		log.debug("Opening garage door")
+    	openGarageDoor()
+        runIn(2, validateGarageOpening)
+    } else {
+    	log.debug("Garage door already open. Not opening.")
+    }
 }
 
 def switchOffHandler(evt) {
-	log.trace "switchOffHandler()"
-	logCurrentState()
-	log.debug "Closing garage door"
-    closeGarageDoor();
+	log.trace("switchOffHandler()")
+    
+    if (getGarageDoorState() != "closed") {
+		log.debug("Closing garage door")
+    	closeGarageDoor();
+        runIn(2, validateGarageClosing)
+    } else {
+    	log.debug("Garage door already closed. Not closing.")
+    }
 }
 
-def doorHandler(evt) {
-	log.trace "doorHandler()"
-	logCurrentState()
+def doorOpenHandler(evt) {
+	log.trace("doorOpenHandler()")
     
-    if (evt.value == "closed") {
-    	log.debug "DEBUG Garage door is closed"
-    } else if (evt.value == "open") {
-    	log.debug "DEBUG Garage door is open"
-        detectDoorState()
-    }
+    syncSwitch()
+    
+    detectDoorState()
+}
+
+def doorClosedHandler(evt) {
+	log.trace("doorClosedHandler()")
+    
+    syncSwitch()
+}
+
+def doorOpeningHandler(evt) {
+	log.trace("doorOpeningHandler()")
+}
+
+def doorClosingHandler(evt) {
+	log.trace("doorClosingHandler()")
+}
+
+def doorUnknownHandler(evt) {
+	log.trace("doorUnknownHandler()")
 }
 
 // =======
 
-def logCurrentState() {
-	log.trace ("logCurrentState()")
+def closeGarageDoor() {
+	log.trace("closeGarageDoor()")
+    
+	thedoor.close()
+}
+
+def openGarageDoor() {
+	log.trace("openGarageDoor()")
+
+	thedoor.open()
+}
+
+def detectDoorState() {
+	log.trace("detectDoorState()")
+    
+    if (getGarageDoorState() == "open" && settings.detectOpenTimeout != null) {
+   		log.debug("Submitting open check in " + settings.detectOpenTimeout)
+       	runIn(settings.detectOpenTimeout, checkDoor)
+   	}
+}
+
+def checkDoor() {
+	log.trace("checkDoor()")
+    
+    if (thedoor.currentState("door").value == "open") {
+    	log.debug("Garage door still open")
+        log.debug("sendPushMessage is " + settings.sendPushMessage)
+        if (settings.sendPushMessage != "No") {
+        	log.debug("Sending push message")
+        	sendPush("Relay: Garage door left open")
+    	} else {
+        	log.debug("Push messages disabled")
+        }
+    }
+}
+
+def getGarageDoorState() {
+	log.trace("getGarageDoorState()")
     
 	def currentState = "unknown"
 	def theDoorState = thedoor.currentState("door")
@@ -101,41 +185,29 @@ def logCurrentState() {
     	currentState = theDoorState.value
     }
     
-    log.debug "Garage door is currently " + currentState
-}
-
-def checkDoor() {
-	log.trace("checkDoor()")
+    log.debug("Garage door is currently " + currentState)
     
-    if (thedoor.currentState("door").value == "closed") {
-    	log.debug "Garage door still open"
-        
-        if (sendPushMessage != "No") {
-        	log.debug("Sending push message")
-        	//sendPush("Garage door left open")
-    	}
-    }
+    return currentState
 }
 
-def closeGarageDoor() {
-	thedoor.close()
-}
-
-def openGarageDoor() {
-	thedoor.open()
-}
-
-def detectDoorState() {
-	log.trace ("detectDoorState()")
+def validateGarageOpening() {
+	log.trace("validateGarageOpening()")
     
-    if (sendPushMessage != "No") {
-	    if (thedoor.currentState("door").value == "open") {
-    		log.debug "Garage door is open"
-        	runIn(detectOpenTimeout, checkDoor)
-    	} else {
-        	log.debug "Garage door is closed"
-        }
+    validateGarageState("opening")
+}
+
+def validateGarageClosing() {
+	log.trace("validateGarageClosing()")
+    
+    validateGarageState("closing")
+}
+
+def validateGarageState(state) {
+	log.trace("validateGarageState(\"" + state + "\")" )
+    
+    if (getGarageDoorState() != state) {
+    	sendPush("Relay: Garage door not " + state)
     } else {
-    	log.debug "Push messages turned off"
+    	log.debug("Garage door is now " + state)
     }
 }
